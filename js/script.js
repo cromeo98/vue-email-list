f0100422e2058c807f9f95da5908c304dcffec8e
cromeo98 <romeo.christian98@gmail.com>
2021-07-06 17:07:00 +0200

    add: axios call to api

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ const app = new Vue(
         el: '#root',
         data: {
 
+        },
+        mounted() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (result){
+                console.log(result);
+            }) 
         }
     }
 );
