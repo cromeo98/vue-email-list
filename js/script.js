1f03c612f4183b9fe4c2725d4a981e32d35e99a6
cromeo98 <romeo.christian98@gmail.com>
2021-07-06 17:15:50 +0200

    push emails in emaillist

--- js/script.js
+++ js/script.js
@@ -2,16 +2,22 @@ const app = new Vue(
     {
         el: '#root',
         data: {
+            eMailList: [
 
+            ]
         },
         mounted() {
             // axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (result){
             //     console.log(result);
             // }) 
             // -------OR-------
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(result => {
-                console.log(result.data.response);
-            })
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(result => {
+                    this.eMailList.push(result.data.response);
+                })
+            }
+            console.log(this.eMailList);
+
         }
     }
 );
